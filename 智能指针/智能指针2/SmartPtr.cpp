fc4996d75d0a63fdfb4c6d29d8f8ad46ebc1521a
Zxiaoxue <18829897183@163.com>
2016-09-03 10:13:36 +0800

    second commit

---
+++ 智能指针/智能指针2/SmartPtr.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include "SmartPtr.hpp"
+using namespace std;
+
+int main()
+{
+	Test1();
+	//Test2();
+	//Test3();
+	system("pause");
+	return 0;
+}
